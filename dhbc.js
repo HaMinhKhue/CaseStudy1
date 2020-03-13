965efe9f00846edd0d1e3339c6484880cd507184
HaMinhKhue <khuesilicat@gmail.com>
2020-03-13 11:22:12 +0700

    .

--- dhbc.js
+++ dhbc.js
@@ -1,58 +1,86 @@
-originlist=['aimo','aomua','bikich','dauthu','homcongduc','kiemchuyen','mahoa','nhaccu','nhanduc','sotroi'];
-chosenlist=[]
-rightanswerlist=[]
-round=0
-iteration=[]
+
+function setUpLevel1() {
+    originlist=['aimo','aomua','bikich','dauthu','homcongduc','kiemchuyen','mahoa','nhaccu','nhanduc','sotroi'];
+    chosenlist=[];
+    rightanswerlist=[];
+    round=0;
+    iteration=[]
+}
 
 function turnOffNofication() {
     document.getElementById('nofication').innerHTML=''
 }
 function refresh() {
-    document.getElementById("image").src = './images/'+chosen + ".jpg"
-    turnOffNofication()
-    document.getElementById('sochucai').innerHTML=chosen.length
+    document.getElementById("image").src = './images/'+chosen + ".jpg";
+    turnOffNofication();
+    document.getElementById('suggestion').innerHTML=''
     document.getElementById('answer').value=''
 }
 
 function choosenew() {
-    i=Math.round(Math.random()*(originlist.length-1))
-    chosen=originlist[i]
-    originlist.splice(i,1)
-    chosenlist.push(chosen)
-    document.getElementById('serial').innerHTML = 'Câu số ' + ++round
+    i=Math.round(Math.random()*(originlist.length-1));
+    chosen=originlist[i];
+    originlist.splice(i,1);
+    chosenlist.push(chosen);
+    document.getElementById('serial').innerHTML = 'Câu số ' + ++round;
     refresh()
 
 }
 function start() {
-    document.getElementById('start').innerHTML = ''
-    document.getElementById('serial').style = ''
-    document.getElementById('image').style = ''
-    document.getElementById('main').style = ''
+    do {
+        playername=prompt('Đăng ký tên người chơi (bắt buộc):')
+    } while (playername===null);
+    setUpLevel1()
+    document.getElementById('start').innerHTML = '';
+    document.getElementById('serial').style = '';
+    document.getElementById('image').style = '';
+    document.getElementById('main').style = '';
 
-    choosenew()
-
+    choosenew();
+console.log(playername)
 }
 function check() {
-    switch (document.getElementById('answer').value) {
-        case '':document.getElementById('nofication').innerHTML ='Bạn chưa nhập đáp án';break
-        case chosen:document.getElementById('nofication').innerHTML = "Chuẩn, bạn được cộng thêm 1 điểm!";
+    answer=change_alias(document.getElementById('answer').value);
+    switch (answer) {
+        case '':document.getElementById('nofication').innerHTML ='Bạn chưa nhập đáp án';break;
+        case chosen:document.getElementById('nofication').innerHTML = "Chuẩn!";
             if(rightanswerlist.includes(chosen)===false){
-                rightanswerlist.push(chosen)
+                rightanswerlist.push(chosen);
                 document.getElementById('scores').innerHTML=rightanswerlist.length;
-                if(rightanswerlist.length>9&&rightanswerlist.includes('aimo')){
+                setTimeout(function () {
+                    switch (rightanswerlist.length) {
+                        case 1:document.getElementById('nofication').innerHTML=playername+' vừa lập chiến công đầu!';break;
+                        case 2:document.getElementById('nofication').innerHTML=playername+' vừa có thêm 1 chiến công!';break;
+                        case 3:document.getElementById('nofication').innerHTML=playername+' vừa lập hat-trick!';break;
+                        case 4:document.getElementById('nofication').innerHTML=playername+' đang cày nát trò chơi này!';break;
+                        case 5:document.getElementById('nofication').innerHTML=playername+' thật không thể ngăn cản!';break;
+                        case 6:document.getElementById('nofication').innerHTML=playername+' đang áp đảo tuyệt đối!';break;
+                        case 7:document.getElementById('nofication').innerHTML=playername+' thật thần thánh!';break;
+                        case 9:document.getElementById('nofication').innerHTML=playername+' là truyền thuyết!';break;
+                        case 8:document.getElementById('nofication').innerHTML=playername+' đã trở thành huyền thoại!';break;
+                        case 10:document.getElementById('nofication').innerHTML=playername+' là đấng tối cao!';break
+                    }
+                },1000)
+                if(rightanswerlist.length>9/*&&rightanswerlist.includes('aimo')*/){
                     setTimeout(function () {
-                        alert('Bạn đã ghi điểm tuyệt đối, mời sang cấp độ 2!')
+                        if(confirm('Bạn đã phá đảo cấp 1, chuyển sang chơi cấp 2?')){
+                            nextlevel()
+                        }
                     },1000)
                 }
-                if(rightanswerlist.length>9&&rightanswerlist.includes('cacao')){
+                if(rightanswerlist.length>9/*&&rightanswerlist.includes('cacao')*/){
                     setTimeout(function () {
-                        alert('Bạn đã phá đảo cấp độ 2!')
+                        alert('Xin chúc mừng, bạn đã phá đảo cấp 2!')
                     },1000)
                 }
-            }break
+            }break;
         default:document.getElementById('nofication').innerHTML = "Không đúng, mời bạn thử lại!"
 
     }
+}
+function showTheChosen() {
+    document.getElementById('nofication').innerHTML='Đáp án đúng là: '+chosen
+
 }
 function next() {
    switch (iteration.length===0) {
@@ -60,9 +88,9 @@ function next() {
            choosenew()
        }
        break;
-       default:chosen=iteration[iteration.length-1]
-           chosenlist.push(iteration.pop())
-           document.getElementById('serial').innerHTML = 'Câu số ' + ++round
+       default:chosen=iteration[iteration.length-1];
+           chosenlist.push(iteration.pop());
+           document.getElementById('serial').innerHTML = 'Câu số ' + ++round;
            refresh()
    }
 }
@@ -70,31 +98,32 @@ function next() {
 
 function previous() {
     if (chosenlist.length>1){
-        iteration.push(chosenlist.pop())
-        chosen=chosenlist[chosenlist.length-1]
+        iteration.push(chosenlist.pop());
+        chosen=chosenlist[chosenlist.length-1];
         document.getElementById('serial').innerHTML = 'Câu số ' + --round
         refresh()
     }
 }
 function reset() {
-    location.reload()
-
+    if(confirm('Vẫn sử dụng tên hiện tại?')){setUpLevel1();
+        document.getElementById('scores').innerHTML=0;
+        choosenew()}else {start()}
 }
 function setUpLevel2() {
     originlist=['cacao','nhosi','canhdong','nhatky','lenmang','giaotranh','baogia','cuphap','cuuvan','congcong'];
-    chosenlist=[]
-    rightanswerlist=[]
-    round=0
+    chosenlist=[];
+    rightanswerlist=[];
+    round=0;
     iteration=[]
 
 }
 function nextlevel() {
     if(rightanswerlist.length>2){
         setTimeout(function () {
-            document.getElementById('h2').innerHTML='Game đuổi hình bắt chữ 2020- Cấp độ 2'
+            document.getElementById('h2').innerHTML='Game đuổi hình bắt chữ 2020- Cấp độ 2';
             document.getElementById('scores').innerHTML=0
-            document.getElementById('nextlevel').replaceWith('Các cấp sau đang được xây dựng')
-            setUpLevel2()
+            document.getElementById('nextlevel').replaceWith('Các cấp sau đang được xây dựng');
+            setUpLevel2();
             choosenew()
         },1000)
     }else {
